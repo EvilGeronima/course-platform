697fc88fc9e619e55ae4dbae71a10ade29081388
PT <20013003@mail.ru>
2022-11-14 21:37:26 +0300

    make scroll

--- js/modal.js
+++ js/modal.js
@@ -1,12 +1,26 @@
 const button = document.getElementById('modalSign')
+
 button?.addEventListener('click'
 	, () => {
 		const modal = document.querySelector('.modal')
-		console.log(modal.style.display);
 		if (modal.style.display === 'none') {
 			modal.style.display = 'flex'
 		} else {
 			modal.style.display = 'none'
+		}
+
+	})
+
+const buttonScroll = document.getElementById('scroll')
+
+
+buttonScroll?.addEventListener('click'
+	, () => {
+		if (scrollY > 0) {
+			window.scroll({
+				top: 0,
+				behavior: 'smooth'
+			})
 		}
 
 	})
